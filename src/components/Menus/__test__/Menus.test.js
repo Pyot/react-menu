4cc8a947da869057491f2f15cd5b2cc056ca0a21
peter <piotr.ludwig@typethink.com>
2019-09-02 19:46:23 +0200

    Test for DesktopMenu Body

--- src/components/Menus/__test__/Menus.test.js
+++ src/components/Menus/__test__/Menus.test.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { render, Simulate, fireEvent } from "@testing-library/react";
+import { render, Simulate, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import DesktopMenu from 'components/Menus/DesktopMenu/DesktopMenuContainer';
+import API from 'mockup-api/API.js'
 
 
-test("render  logo inside desktop menu header", () => {
+test("render header - logo inside desktop menu", () => {
     const { getByAltText } = render (<DesktopMenu/>);
     expect(getByAltText('logo')).toBeTruthy();
 })
 
-test("render  close button inside desktop menu header", () => {
+test("render header - close button inside desktop menu", () => {
     const { getByTestId } = render (<DesktopMenu/>);
     expect(getByTestId('close-button')).toBeTruthy();
+})
+
+test("render body - main and user menu", () => {
+    const { getAllByTestId } = render (<DesktopMenu api={API}/>);
+
+    expect(getAllByTestId('main-menu').length).toEqual(API.desktopMain.length);
+    expect(getAllByTestId('user-menu').length).toEqual(API.desktopMain.length);    
 })
